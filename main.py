3a1b7364abab94d9f9b7daaa4f0101bc4f0e5c08
Anne Jan Brouwer <brouwer@annejan.com>
2019-11-29 10:23:18 +0100

    Nu met achterkant text . .

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-        RaspberryPi Python app FeedMe
+        RaspberryPi Python app Product Label
 
 © 2019 Jolanda Versteeg
 Based on a demo application by Anne Jan Brouwer
@@ -8,34 +8,14 @@ Based on a demo application by Anne Jan Brouwer
 Choose some words, get a nice fitting product label.
 Double click the label to reset the app.
 """
-
 import random
 import sys
-# pylint: disable=import-error
 from PyQt5.QtWidgets import QWidget, QPushButton, QApplication, QLabel
-from PyQt5.QtGui import QPixmap, QFont
+from PyQt5.QtGui import QFont, QPixmap
 from PyQt5.QtCore import Qt
-
-QUESTIONS = 7
-
-LIST_A = [
-    'mooi', 'fijn', 'gaaf', 'geweldig', 'goed', 'jofel', 'keurig', 'kostelijk', 'netjes',
-    'prima', 'reuze', 'uitstekend', 'perfect', 'fantastisch'
-]
-LIST_B = [
-    'lelijk', 'afgrijselijk', 'afschuwelijk', 'afstotelijk', 'afzichtelijk', 'gruwelijk',
-    'mismaakt', 'misvormd', 'onesthetisch', 'onfraai', 'onknap', 'onooglijk', 'wanstaltig',
-    'weerzinwekkend', 'mottig'
-]
-
-INTRO = 'Klik hier om te beginen, klik dan vervolgens je huidige humeur bij elkaar'
-PAYOFF = 'Je koos voornamelijk %s dingen!'
-WORD_A = 'mooie'
-WORD_B = 'lelijke'
-TITLE = 'Mooi %s Lelijk %s'
+import product
 
 
-# pylint: disable=too-many-instance-attributes
 class FeedMe(QWidget):
     """Feed me some data and I'll show you a nice label"""
     def __init__(self):
@@ -45,13 +25,14 @@ class FeedMe(QWidget):
         self.a_chosen = 0
         self.b_chosen = 0
         self.which_list = False
+        self.show_front = True
         self.label_intro = QLabel(self)
         self.label_intro.move(20, 20)
         self.label_intro.setFont(QFont('SansSerif', 32))
         self.label_intro.setMaximumWidth(440)
         self.label_intro.setMinimumWidth(440)
         self.label_intro.setWordWrap(True)
-        self.label_intro.setText(INTRO)
+        self.label_intro.setText(product.INTRO)
         self.label_intro.setAlignment(Qt.AlignCenter)
         self.label_intro.hide()
         self.label_intro.mousePressEvent = self.start_question
@@ -69,7 +50,7 @@ class FeedMe(QWidget):
         self.button2.clicked.connect(lambda: self.button_clicked(2))
 
         self.label_image = QLabel(self)
-        self.label_image.setPixmap(QPixmap("background.jpg"))
+        self.label_image.setPixmap(QPixmap(product.IMAGE))
         self.label_image.hide()
         self.label_text = QLabel(self)
         self.label_text.move(20, 20)
@@ -80,6 +61,20 @@ class FeedMe(QWidget):
         self.label_text.setWordWrap(True)
         self.label_text.hide()
         self.label_text.mouseDoubleClickEvent = self.reset_app
+        self.label_back = QLabel(self)
+        self.label_back.move(20, 20)
+        self.label_back.setFont(QFont('SansSerif', 16))
+        self.label_back.setStyleSheet('color: darkblue')
+        self.label_back.setMaximumWidth(460)
+        self.label_back.setMinimumWidth(460)
+        self.label_back.setWordWrap(True)
+        self.label_back.hide()
+        self.label_back.mouseDoubleClickEvent = self.reset_app
+        self.flip_button = QPushButton("<", self)
+        self.flip_button.move(440, 260)
+        self.flip_button.resize(40, 40)
+        self.flip_button.hide()
+        self.flip_button.clicked.connect(self.toggle_label)
 
         self.setGeometry(0, 0, 480, 300)
         self.setWindowTitle('Mooie demo app')
@@ -93,8 +88,11 @@ class FeedMe(QWidget):
             self.question = 0
             self.a_chosen = 0
             self.b_chosen = 0
+            self.show_front = True
             self.label_image.hide()
             self.label_text.hide()
+            self.label_back.hide()
+            self.flip_button.hide()
             self.button1.hide()
             self.button2.hide()
             self.label_intro.show()
@@ -113,9 +111,9 @@ class FeedMe(QWidget):
             self.a_chosen += 1
         else:
             self.b_chosen += 1
-        self.setWindowTitle(TITLE % (self.a_chosen, self.b_chosen))
+        self.setWindowTitle(product.TITLE % (self.a_chosen, self.b_chosen))
         self.question += 1
-        if self.question < QUESTIONS:
+        if self.question < product.QUESTIONS:
             self.update_question()
         else:
             self.show_label()
@@ -123,18 +121,31 @@ class FeedMe(QWidget):
     def update_question(self):
         """Choose two new words"""
         self.which_list = bool(random.getrandbits(1))
-        self.button1.setText(random.choice((LIST_A, LIST_B)[self.which_list]).title())  # .title() capitalised word
-        self.button2.setText(random.choice((LIST_B, LIST_A)[self.which_list]).title())
+        self.button1.setText(random.choice((product.LIST_A, product.LIST_B)[self.which_list]).title())
+        self.button2.setText(random.choice((product.LIST_B, product.LIST_A)[self.which_list]).title())
 
     def show_label(self):
         """Show payoff label"""
-        text = PAYOFF % (WORD_A, WORD_B)[self.a_chosen < self.b_chosen]
+        text = product.PAYOFF % (product.WORD_A, product.WORD_B)[self.a_chosen < self.b_chosen]
+        back_text = product.BACK_TEXT % (product.WORD_A, product.WORD_B)[self.a_chosen < self.b_chosen]
         self.button1.hide()
         self.button2.hide()
         self.label_image.show()
         self.label_text.setText(text)
+        self.label_back.setText(back_text)
         self.label_text.show()
+        self.flip_button.show()
         self.setWindowTitle(text)
+
+    def toggle_label(self):
+        """Toggle between front and back of label"""
+        self.show_front = not self.show_front
+        if self.show_front:
+            self.label_text.show()
+            self.label_back.hide()
+        else:
+            self.label_text.hide()
+            self.label_back.show()
 
 
 # Start the FeedMe app and quit nicely when Ctrl+Q pressed
---
+++ product.py
@@ -0,0 +1,28 @@
+"""
+Product config file for Product Label App
+
+© 2019 Jolanda Versteeg
+Based on a demo application by Anne Jan Brouwer
+"""
+QUESTIONS = 7
+
+LIST_A = [
+    'mooi', 'fijn', 'gaaf', 'geweldig', 'goed', 'jofel', 'keurig', 'kostelijk', 'netjes',
+    'prima', 'reuze', 'uitstekend', 'perfect', 'fantastisch'
+]
+LIST_B = [
+    'lelijk', 'afgrijselijk', 'afschuwelijk', 'afstotelijk', 'afzichtelijk', 'gruwelijk',
+    'mismaakt', 'misvormd', 'onesthetisch', 'onfraai', 'onknap', 'onooglijk', 'wanstaltig',
+    'weerzinwekkend', 'mottig'
+]
+
+INTRO = 'Klik hier om te beginen, klik dan vervolgens je huidige humeur bij elkaar'
+PAYOFF = 'Je koos voornamelijk %s dingen!'
+WORD_A = 'mooie'
+WORD_B = 'lelijke'
+TITLE = 'Mooi %s Lelijk %s'
+IMAGE = 'background.jpg'
+BACK_TEXT = 'Dit product bevat %s dingen, daarnaast bevat het natuurlijk ook ingrediënten en heeft het voedingswaarde.\n' \
+            'Sommige mensen kunnen hun geluk niet op wanneer ze dit product consumeren.\n' \
+            'Andere mensen vinden het maar zo-zo . .\n' \
+            'Er is ook een aanbevolen dagelijkse hoeveelheid echter publiceren wij die slechts op onze website.'
